1f0f011f62b5b5c9ece89e84e26cf2f3a0c2d0d8
Vitor Darezzo <vittor.darezzo@gmail.com>
2025-08-27 11:53:37 -0300

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,25 @@
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
+
+function showRegister() {
+  loginForm.classList.add('hidden');
+  registerForm.classList.remove('hidden');
+}
+
+function showLogin() {
+  registerForm.classList.add('hidden');
+  loginForm.classList.remove('hidden');
+}
+
+function validateRegister(event) {
+  event.preventDefault();
+  const senha = document.getElementById("reg-senha").value;
+  const confirmarSenha = document.getElementById("confirm-senha").value;
+
+  if (senha !== confirmarSenha) {
+    alert("As senhas não coincidem!");
+    return;
+  }
+
+  alert("Cadastro realizado com sucesso!");
+}
