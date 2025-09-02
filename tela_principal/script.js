6866f92aa11cc8b179c68b0729976daf42f755cf
David Avelar <94112444+DavidAvelar03@users.noreply.github.com>
2025-09-02 15:40:34 -0300

    Update script.js

--- tela_principal/script.js
+++ tela_principal/script.js
@@ -2,3 +2,10 @@ function buscar() {
       const termo = document.querySelector('input').value;
       alert("Você pesquisou por: " + termo);
     }
+ function abrirCarrinho() {
+      document.getElementById('carrinho').classList.add('aberto');
+    }
+
+    function fecharCarrinho() {
+      document.getElementById('carrinho').classList.remove('aberto');
+    }
